311c3e128b4db5cea8454977fb69447b7b7cff9d
ajarian <adrian.william.jones@gmail.com>
2023-06-08 16:42:41 -0400

    setup

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,40 @@
+import { ChakraProvider } from '@chakra-ui/react';
+import { extendTheme } from '@chakra-ui/react';
+
 import '@/styles/globals.css'
 
+const colors = {
+  black: {
+    500: '#020314',
+    400: '#1B1C2C',
+    300: '#353543',
+    200: '#676872',
+    100: '#9A9AA1'
+  },
+  gold: {
+    500: '#E4B416',
+  },
+  ivory: {
+    500: '#EFECE2'
+  },
+  pink: {
+    600: '#C80064',
+    500: '#DE006F'
+  },
+  purple: {
+    600: '#512D9B',
+    500: '#5A32AC',
+    400: '#8532AC',
+    300: '#B332BE',
+  }
+};
+
+const theme = extendTheme({ colors });
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
---
+++ src/pages/support.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SupportPage = () => (
+  <div></div>
+);
+
+export default SupportPage;
